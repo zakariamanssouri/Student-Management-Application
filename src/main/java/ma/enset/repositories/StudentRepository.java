f6e14e61283f2948d02f77d4fd0f9671fa615879
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-15 16:35:44 +0000

    add method for returning paginated students

--- src/main/java/ma/enset/repositories/StudentRepository.java
+++ src/main/java/ma/enset/repositories/StudentRepository.java
@@ -1,7 +1,13 @@
 package ma.enset.repositories;
 
 import ma.enset.entities.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    Page<Student> findByNameContains(String string, org.springframework.data.domain.Pageable pageable);
 }

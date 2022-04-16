88960f7a6ba868e5dc8f3a8da70bcf7deb024c72
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-16 02:34:41 +0000

    add Implementation for UserDetailsService

---
+++ src/main/java/ma/enset/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,47 @@
+package ma.enset.security.service;
+
+import lombok.AllArgsConstructor;
+import ma.enset.security.entities.AppUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Service("userservice")
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private SecurityService securityService;
+
+    public UserDetailsServiceImpl(SecurityService securityService) {
+        this.securityService = securityService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = securityService.loadUserByUsername(username);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        /*mapping */
+        appUser.getAppRoles().forEach(appRole -> {
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appRole.getRoleName());
+            authorities.add(authority);
+        });
+
+        /*second way*/
+      /*  Collection<GrantedAuthority> authorities1 =
+                appUser.getAppRoles().stream().map(role -> {
+                    new SimpleGrantedAuthority(role.getRoleName());
+                }).collect(Collectors.toList());*/
+
+
+        User user = new User(appUser.getUsername(), appUser.getPassword(), authorities);
+        return user;
+    }
+}

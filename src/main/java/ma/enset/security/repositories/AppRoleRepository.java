2332e7cc4dda5aabb359fae31cc8c56fd8283500
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-16 02:24:52 +0000

    add Role repository

---
+++ src/main/java/ma/enset/security/repositories/AppRoleRepository.java
@@ -0,0 +1,8 @@
+package ma.enset.security.repositories;
+
+import ma.enset.security.entities.AppRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppRoleRepository extends JpaRepository<AppRole,Long> {
+    AppRole findByRoleName(String roleName);
+}

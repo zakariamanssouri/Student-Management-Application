926f721e2d293ad02096969a6b5de6944387487e
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-16 02:20:36 +0000

    add security layer , & creating User entity

---
+++ src/main/java/ma/enset/security/entities/AppUser.java
@@ -0,0 +1,22 @@
+package ma.enset.security.entities;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class AppUser {
+    @Id
+    private String userId;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private Boolean active;
+
+    @ManyToMany(fetch = FetchType.EAGER) /*load users with roles */
+    private List<AppRole> appRoles = new ArrayList<>();
+}

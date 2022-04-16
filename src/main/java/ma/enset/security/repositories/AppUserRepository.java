5beaa4b94e67b95797b8f558bbd781ee3cf3af21
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-16 02:23:49 +0000

    add User repository

---
+++ src/main/java/ma/enset/security/repositories/AppUserRepository.java
@@ -0,0 +1,8 @@
+package ma.enset.security.repositories;
+
+import ma.enset.security.entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser,String> {
+    AppUser findByUsername(String username);
+}

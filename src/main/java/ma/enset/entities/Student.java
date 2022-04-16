0dd9e2f34f8d9ad996cef93654131189f37bcf42
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-16 02:13:49 +0000

    add some constraints on fields

--- src/main/java/ma/enset/entities/Student.java
+++ src/main/java/ma/enset/entities/Student.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 
@@ -16,12 +19,18 @@ import java.util.Date;
 public class Student {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty(message = "the username should not be empty")
+    @Size(min = 3, max = 255)
     private String name;
+
+    @NotEmpty(message = "the lastname should not be empty")
+    @Size(min = 3, max = 255)
     private String lastName;
     @Column(unique = true)
     private String email;
     @Temporal(TemporalType.DATE)
-    @DateTimeFormat(pattern = "yyyy-mm-jj")
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
     private Date birthDate;
     private Gender gender;
     private boolean enRegle;
